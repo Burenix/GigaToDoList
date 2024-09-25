575d9660bdab7a5c3bcb0a9630f06ff42030e055
Burenix <antosha.kolyuchkin@gmail.com>
2024-09-25 18:42:23 +0300

    add function to view tasks list

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from .views import TaskList
+
 urlpatterns = [
-    
+    path('main/', TaskList.as_view(), name='tasks'),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import edit
+from django.views import generic
+
+from .models import Task
 
 # Create your views here.
+class TaskList(generic.ListView):
+    model = Task
+    template_name = 'todo_list/main.html'
+    context_object_name = 'tasks'
