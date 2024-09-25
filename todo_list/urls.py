e7d5b535c1705584a0c11b6dd4ee1efaff27ce78
Burenix <antosha.kolyuchkin@gmail.com>
2024-09-25 19:01:18 +0300

    add function to create task

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import TaskList
+from .views import TaskCreate, TaskList
 
 urlpatterns = [
     path('main/', TaskList.as_view(), name='tasks'),
+    path('main/create-task/', TaskCreate.as_view(), name='task-create'),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import edit
 from django.views import generic
 
@@ -9,3 +10,8 @@ class TaskList(generic.ListView):
     model = Task
     template_name = 'todo_list/main.html'
     context_object_name = 'tasks'
+
+class TaskCreate(generic.CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
