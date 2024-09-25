2f46c767e44afe638376392193d55c557709e787
Burenix <antosha.kolyuchkin@gmail.com>
2024-09-25 18:18:27 +0300

    create model in database for tasks

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=52)
+    description = models.TextField(max_length=252)
+    status = models.BooleanField(default=False)
+    deadline = models.DateTimeField()
+    task_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['status']
