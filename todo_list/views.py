3ba1b362a20a8f245db36fc3592c1b338f942615
Burenix <antosha.kolyuchkin@gmail.com>
2024-09-27 13:46:44 +0300

    Add function to search tasks

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import edit
@@ -5,7 +6,8 @@ from django.views import generic
 
 from .models import Task
 
-# Create your views here.
+################### CRUD ###################
+
 class TaskList(generic.ListView):
     model = Task
     template_name = 'todo_list/main.html'
@@ -27,3 +29,22 @@ class TaskUpdate(generic.UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
+
+################### CRUD ###################
+
+################## SEARCH ##################
+
+class Search(generic.ListView):
+    template_name = 'todo_list/main.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        return Task.objects.filter(title__icontains=
+                                   self.request.GET.get('search_text'))
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
+    
+################## SEARCH ##################
