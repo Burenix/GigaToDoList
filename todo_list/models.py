85421a20c454b3377782fef2b6e10384244c87bb
Burenix <antosha.kolyuchkin@gmail.com>
2024-10-07 22:25:05 +0300

    Add authentication for tasks and add new field in task model

--- todo_list/models.py
+++ todo_list/models.py
@@ -2,6 +2,14 @@ from datetime import timedelta
 from django.db import models
 from django.conf import settings
 
+COMPLEXITY_CHOICES = (
+    (1, 'Easy'),
+    (2, 'Medium'),
+    (3, 'Hard'),
+    (4, 'Very hard'),
+    (5, 'Impossible'),
+)
+
 # Create your models here.
 class Task(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tasks')
@@ -11,6 +19,7 @@ class Task(models.Model):
     deadline = models.DateTimeField()
     reminder_time = models.DurationField(default=timedelta(hours=1))
     task_created = models.DateTimeField(auto_now_add=True)
+    complexity = models.IntegerField(choices=COMPLEXITY_CHOICES)
 
     def __str__(self) -> str:
         return self.title
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -5,33 +6,67 @@ from todo_list.models import Task
 
 ################### CRUD ###################
 
-class TaskList(generic.ListView):
+class TaskList(LoginRequiredMixin, generic.ListView):
     model = Task
     template_name = 'todo_list/main.html'
     context_object_name = 'tasks'
 
-class TaskCreate(generic.CreateView):
+    def get_queryset(self):
+        """
+        Возвращает список задач только для текущего авторизованного пользователя.
+        """
+        return Task.objects.filter(user=self.request.user)
+
+class TaskCreate(LoginRequiredMixin, generic.CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complexity', 'deadline', 'reminder_time']
     success_url = reverse_lazy('tasks')
 
-class TaskDetail(generic.DetailView):
+    def form_valid(self, form):
+        """
+        При успешном создании задачи устанавливает пользователя, который создал задачу.
+        """
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class TaskDetail(LoginRequiredMixin, UserPassesTestMixin, generic.DetailView):
     model = Task
 
-class TaskDelete(generic.DeleteView):
+    def test_func(self):
+        """
+        Проверяет, совпадает ли пользователь, просматривающий детальную информацию о задаче, с пользователем, 
+        который создал эту задачу.
+        """
+        return super().get_object().user == self.request.user
+
+class TaskDelete(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
     model = Task
     success_url = reverse_lazy('tasks')
 
-class TaskUpdate(generic.UpdateView):
+    def test_func(self):
+        """
+        Проверяет, совпадает ли пользователь, удаляющий задачу, с пользователем, 
+        который создал эту задачу.
+        """
+        return super().get_object().user == self.request.user
+
+class TaskUpdate(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'status', 'deadline', 'reminder_time']
     success_url = reverse_lazy('tasks')
+
+    def test_func(self):
+        """
+        Проверяет, совпадает ли пользователь, обновляющий задачу, с пользователем, 
+        который создал эту задачу.
+        """
+        return super().get_object().user == self.request.user
 
 ################### CRUD ###################
 
 ################## SEARCH ##################
 
-class Search(generic.ListView):
+class Search(LoginRequiredMixin, generic.ListView):
     template_name = 'todo_list/main.html'
     context_object_name = 'tasks'
 
@@ -48,14 +83,14 @@ class Search(generic.ListView):
 
 ################## FILTER ##################
 
-class TaskFilter(generic.ListView):
+class TaskFilter(LoginRequiredMixin, generic.ListView):
     template_name = 'todo_list/main.html'
     context_object_name = 'tasks'
 
     def get_queryset(self):
         queryset = Task.objects.all()
 
-        filter = self.request.GET.get('filter')
+        filter = self.request.GET.get('filter', 'all') 
 
         if filter == 'complete':
             return queryset.filter(status__exact=True)
@@ -63,6 +98,10 @@ class TaskFilter(generic.ListView):
             return queryset.filter(status__exact=False)
         elif filter == 'deadline':
             return queryset.filter(status__exact=False).order_by('deadline')
+        elif filter == 'simple':
+            return queryset.filter(status__exact=False).order_by('complexity')
+        elif filter == 'difficult':
+            return queryset.filter(status__exact=False).order_by('-complexity')
         else:
             return queryset  
 
